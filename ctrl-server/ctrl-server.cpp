dd978ea0f307b24c7345107e19487631e3e663ac
Seth Traverse <seth@viaware.ca>
2016-10-06 00:26:33 -0700

    Added ctrl-server skeleton

---
+++ ctrl-server/ctrl-server.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#define VERSION "0.0.1"
+
+int main(int argc, char **argv)
+{
+    std::cout << "Running ViaWare Control Server v" << VERSION << std::endl;
+
+    return 0;
+}
