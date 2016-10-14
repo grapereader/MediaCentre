3ea50d505948078c6694dab2fe6522a947b083ef
Seth Traverse <seth@viaware.ca>
2016-10-13 19:44:53 -0700

    Start adding some frontend stuff

--- ctrl-server/backend/ControlServer.cpp
+++ ctrl-server/backend/ControlServer.cpp
@@ -29,28 +29,7 @@ int main(int argc, char **argv)
     auto server = std::unique_ptr<vmc::HTTPServer>(new vmc::HTTPServer("0.0.0.0", 8080));
     auto router = std::unique_ptr<vmc::Router>(new vmc::Router(server.get()));
     
-    router->routeStaticFile("/test", "./HTTPServer.cpp");
-    router->routeStaticFolder("/test2", "../");
-    router->route({vmc::method::GET}, "/test3", [](vmc::HTTPRequest &request, std::vector<std::string> const &urlParts, std::unordered_map<std::string, std::string> const &urlParams)
-            {
-                std::cout << "Routing of /test3 works" << std::endl;
-
-                for (auto it = urlParts.begin(); it != urlParts.end(); it++)
-                {
-                    std::cout << *it << " -> ";
-                }
-                std::cout << std::endl;
-
-                for (auto it = urlParams.begin(); it != urlParams.end(); it++)
-                {
-                    std::cout << it->first << " -> " << it->second << std::endl;
-                }
-
-                vmc::HTTPHeaders response;
-                request.sendResponseHeaders(200, response);
-
-                request.getStream() << "Yay for routing!\r\n";
-            });
+    router->routeStaticFolder("/client", "../frontend");
 
     router->start();
 
