fcafa814ad8f9746f4cbc6dc6529e755389b62c3
WinMac32 <seth@viaware.ca>
2016-10-16 15:14:41 -0700

    Move application logic into its own file

--- ctrl-server/backend/ControlServer.cpp
+++ ctrl-server/backend/ControlServer.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <memory>
 
-#include "HTTPServer.h"
-#include "Router.h"
+#include "Application.h"
 
 #define VERSION "0.0.1"
 
@@ -10,30 +8,7 @@ int main(int argc, char **argv)
 {
     std::cout << "Running ViaWare Control Server v" << VERSION << std::endl;
 
-    auto server = std::unique_ptr<vmc::HTTPServer>(new vmc::HTTPServer("0.0.0.0", 8080));
-    auto router = std::unique_ptr<vmc::Router>(new vmc::Router(server.get()));
-    router->routeStaticFolder("/client", "../frontend");
-
-    router->route({vmc::method::GET}, "/test", [](vmc::HTTPRequest &request, std::vector<std::string> const &urlParts, std::unordered_map<std::string, std::string> const &urlParams)
-    {
-        auto session = request.initSession();
-        std::string greeting = "Meh. Strangers are lame...";
-        if (session->exists("greeting"))
-        {
-            greeting = session->get<std::string>("greeting");
-        }
-        std::string resp = "Hello there... <p>";
-        resp.append(greeting);
-
-        session->put<std::string>("greeting", "Hey, you've been here before!");
-
-        request.getResponseHeaders()->put("Content-Length", resp.length());
-        request.sendResponseHeaders(200);
-
-        request.getStream() << resp << "\r\n";
-    });
-
-    router->start();
+    vmc::start();
 
     return 0;
 }
