2bc08e00b586a4c27cb470504c5fa90bd9eca432
Seth Traverse <seth@viaware.ca>
2016-11-01 20:35:41 -0700

    Initialized barebones angular setup

---
+++ ctrl-server/frontend/app/components/app.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'application',
+  template: '<h1>ViaWare Media Centre</h1>'
+})
+export class AppComponent { }
---
+++ ctrl-server/frontend/app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './modules/app';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
