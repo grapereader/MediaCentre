b1e7e21f6fc2ba1059c754e4441606311802e881
Seth Traverse <seth@viaware.ca>
2016-11-23 18:38:35 -0800

    Starting to figure out the Angular router

--- ctrl-server/frontend/app/app.module.ts
+++ ctrl-server/frontend/app/app.module.ts
@@ -1,11 +1,26 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login.component';
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports:      [
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                component: HomeComponent
+            },
+            {
+                path: 'login',
+                component: LoginComponent
+            }
+        ])
+    ],
+    declarations: [ AppComponent, LoginComponent, HomeComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ ctrl-server/frontend/app/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: "app/login.template.html"
+})
+export class LoginComponent {
+
+}
